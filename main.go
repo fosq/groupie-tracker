c10c6db739d3a1e47d428a268ccd67def1bb7b88
mvahe <martinvahe@hotmail.com>
2023-02-07 13:10:43 +0200

    Remove front-end, back-end priority!

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
-	"text/template"
+	"os"
 )
 
 type IO struct {
@@ -12,37 +14,48 @@ type IO struct {
 	Output string
 }
 
-type Artists struct {
+type Endpoints struct {
+	Artists   string `json:"artists"`
+	Locations string `json:"locations"`
+	Dates     string `json:"dates"`
+	Relation  string `json:"relation"`
+}
+
+type Artists []struct {
+	ID           int      `json:"id"`
+	Image        string   `json:"image"`
+	Name         string   `json:"name"`
+	Members      []string `json:"members"`
+	CreationDate int      `json:"creationDate"`
+	FirstAlbum   string   `json:"firstAlbum"`
+	Locations    string   `json:"locations"`
+	ConcertDates string   `json:"concertDates"`
+	Relations    string   `json:"relations"`
 }
 
 func main() {
-
-	http.Handle("/templates/", http.FileServer(http.Dir("templates")))
-
-	http.HandleFunc("/", formHandler)
-
-	fmt.Printf("Starting server at port 8080, access the page with 'localhost:8080' in a browser\n")
-	fmt.Printf("Press 'Ctrl + C' to end the server\n")
-	if err := http.ListenAndServe(":8080", nil); err != nil { // Listens on port 8080
-		log.Fatal(err)
-	}
-}
-
-func formHandler(w http.ResponseWriter, r *http.Request) {
-	var io IO
-	whtml, err := template.ParseFiles("templates/index.html")
+	response, err := http.Get("https://groupietrackers.herokuapp.com/api/artists")
 
 	if err != nil {
-		http.Error(w, "404 - Resource not found", http.StatusNotFound)
+		fmt.Print(err.Error())
+		os.Exit(1)
 	}
 
-	if r.URL.Path != "/" {
-		http.Error(w, "404 - Page not found", http.StatusNotFound)
-		return
+	responseData, err := io.ReadAll(response.Body)
+	if err != nil {
+		log.Fatal(err)
 	}
+	//fmt.Println(string(responseData))
 
-	if r.URL.Path == "/" {
-		w.WriteHeader(http.StatusOK)
-		whtml.Execute(w, io)
+	var result Artists
+	json.Unmarshal(responseData, &result)
+
+	//fmt.Println(reflect.TypeOf(&result))
+	fmt.Println(result[0].Name)
+	fmt.Println(len(result[0].Members))
+	fmt.Println(len(result))
+
+	for i := 0; i < len(result); i++ {
+		fmt.Println(result[i].Name)
 	}
 }

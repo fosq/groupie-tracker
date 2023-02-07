f2993073a0f7ae632f689b3026963711f910f0fa
mvahe <martinvahe@hotmail.com>
2023-02-07 16:40:35 +0200

    added back front-end, back-end semi working

--- main.go
+++ main.go
@@ -3,10 +3,11 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"io"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
+	"text/template"
 )
 
 type IO struct {
@@ -33,7 +34,71 @@ type Artists []struct {
 	Relations    string   `json:"relations"`
 }
 
+type Locations struct {
+	Index []struct {
+		ID        int      `json:"id"`
+		Locations []string `json:"locations"`
+		Dates     string   `json:"dates"`
+	} `json:"index"`
+}
+
 func main() {
+	http.Handle("/templates/", http.StripPrefix("/templates", http.FileServer(http.Dir("templates"))))
+
+	http.HandleFunc("/", formHandler) // Handles /ascii-art
+
+	fmt.Printf("Starting server at port 8080, access the page with 'localhost:8080' in a browser\n")
+	fmt.Printf("Press 'Ctrl + C' to end the server\n")
+	if err := http.ListenAndServe(":8080", nil); err != nil { // Listens on port 8080
+		log.Fatal(err)
+	}
+}
+
+func formHandler(w http.ResponseWriter, r *http.Request) {
+	var io IO
+
+	whtml, err := template.ParseFiles("templates/index.html")
+
+	if err != nil {
+		http.Error(w, "404 - Resource not found", http.StatusNotFound)
+	}
+
+	if r.URL.Path != "/" {
+		http.Error(w, "404 - Page not found", http.StatusNotFound)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	whtml.Execute(w, io)
+
+	response, err := http.Get("https://groupietrackers.herokuapp.com/api/artists")
+
+	if err != nil {
+		fmt.Print(err.Error())
+		os.Exit(1)
+	}
+
+	responseData, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	//fmt.Println(string(responseData))
+
+	var result Artists
+	json.Unmarshal(responseData, &result)
+
+	io.Output = result[0].Name
+
+	whtml, err = template.ParseFiles("templates/artistBubble.html")
+	if err != nil {
+		http.Error(w, "404 - Resource not found", http.StatusNotFound)
+	}
+
+	w.WriteHeader(http.StatusOK)
+	whtml.Execute(w, io)
+}
+
+/*func api() {
 	response, err := http.Get("https://groupietrackers.herokuapp.com/api/artists")
 
 	if err != nil {
@@ -55,7 +120,11 @@ func main() {
 	fmt.Println(len(result[0].Members))
 	fmt.Println(len(result))
 
+	// Print all artists
 	for i := 0; i < len(result); i++ {
 		fmt.Println(result[i].Name)
 	}
-}
+	fmt.Println()
+
+	fmt.Printf("Name: %v\nMembers: %v\nCreation Date: %v\nFirst album: %v\n", result[0].Name, result[0].Members, result[0].CreationDate, result[0].FirstAlbum)
+}*/

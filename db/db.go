81c62f91ed30f952936014f6b3753c0f9162f2d0
mvahe <martinvahe@hotmail.com>
2023-02-16 19:33:50 +0200

    MOAR PROGRESS

---
+++ db/db.go
@@ -0,0 +1,28 @@
+package db
+
+type Endpoints struct {
+	Artists   string `json:"artists"`
+	Locations string `json:"locations"`
+	Dates     string `json:"dates"`
+	Relation  string `json:"relation"`
+}
+
+type Artist struct {
+	ID           int      `json:"id"`
+	Image        string   `json:"image"`
+	Name         string   `json:"name"`
+	Members      []string `json:"members"`
+	CreationDate int      `json:"creationDate"`
+	FirstAlbum   string   `json:"firstAlbum"`
+	Locations    string   `json:"locations"`
+	ConcertDates string   `json:"concertDates"`
+	Relations    string   `json:"relations"`
+}
+
+type Locations struct {
+	Index []struct {
+		ID        int      `json:"id"`
+		Locations []string `json:"locations"`
+		Dates     string   `json:"dates"`
+	} `json:"index"`
+}
--- main.go
+++ main.go
@@ -10,19 +10,27 @@ import (
 	"text/template"
 )
 
-type IO struct {
-	Input  string
-	Output []string
+type Card struct {
+	Id           int
+	FirstAlbum   string
+	Location     []string
+	Image        string
+	GroupName    string
+	CreationDate int
+	Members      []string
 }
 
-type Endpoints struct {
-	Artists   string `json:"artists"`
-	Locations string `json:"locations"`
-	Dates     string `json:"dates"`
-	Relation  string `json:"relation"`
+type MainData struct {
+	Cards        []Card
+	CountMembers []int
+	GroupNames   []string
+	CreationDate []int
+	FirstAlbum   []string
+	Members      []string
+	Locations    []string
 }
 
-type Artists struct {
+type Artist struct {
 	ID           int      `json:"id"`
 	Image        string   `json:"image"`
 	Name         string   `json:"name"`
@@ -32,14 +40,6 @@ type Artists struct {
 	Locations    string   `json:"locations"`
 	ConcertDates string   `json:"concertDates"`
 	Relations    string   `json:"relations"`
-}
-
-type Locations struct {
-	Index []struct {
-		ID        int      `json:"id"`
-		Locations []string `json:"locations"`
-		Dates     string   `json:"dates"`
-	} `json:"index"`
 }
 
 func main() {
@@ -55,9 +55,9 @@ func main() {
 }
 
 func formHandler(w http.ResponseWriter, r *http.Request) {
-	var io IO
 
 	whtml, err := template.ParseFiles("templates/index.html")
+	whtml.Execute(w, err)
 
 	if err != nil {
 		http.Error(w, "404 - Resource not found", http.StatusNotFound)
@@ -67,9 +67,6 @@ func formHandler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "404 - Page not found", http.StatusNotFound)
 		return
 	}
-
-	w.WriteHeader(http.StatusOK)
-	whtml.Execute(w, io)
 
 	response, err := http.Get("https://groupietrackers.herokuapp.com/api/artists")
 
@@ -84,13 +81,17 @@ func formHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	//fmt.Println(string(responseData))
 
-	var result []Artists
+	var result []Artist
 	json.Unmarshal(responseData, &result)
 
-	for i := 0; i < len(result); i++ {
-		io.Output = append(io.Output, result[i].Name)
+	data := Card{
+		Id:           result[0].ID,
+		Image:        result[0].Image,
+		GroupName:    result[0].Name,
+		CreationDate: result[0].CreationDate,
+		FirstAlbum:   result[0].FirstAlbum,
+		Members:      result[0].Members,
 	}
-	fmt.Println(io.Output)
 
 	whtml, err = template.ParseFiles("templates/artistBubble.html")
 	if err != nil {
@@ -98,36 +99,5 @@ func formHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	//w.WriteHeader(http.StatusOK)
-	whtml.Execute(w, io)
+	whtml.Execute(w, data)
 }
-
-/*func api() {
-	response, err := http.Get("https://groupietrackers.herokuapp.com/api/artists")
-
-	if err != nil {
-		fmt.Print(err.Error())
-		os.Exit(1)
-	}
-
-	responseData, err := io.ReadAll(response.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
-	//fmt.Println(string(responseData))
-
-	var result Artists
-	json.Unmarshal(responseData, &result)
-
-	//fmt.Println(reflect.TypeOf(&result))
-	fmt.Println(result[0].Name)
-	fmt.Println(len(result[0].Members))
-	fmt.Println(len(result))
-
-	// Print all artists
-	for i := 0; i < len(result); i++ {
-		fmt.Println(result[i].Name)
-	}
-	fmt.Println()
-
-	fmt.Printf("Name: %v\nMembers: %v\nCreation Date: %v\nFirst album: %v\n", result[0].Name, result[0].Members, result[0].CreationDate, result[0].FirstAlbum)
-}*/
